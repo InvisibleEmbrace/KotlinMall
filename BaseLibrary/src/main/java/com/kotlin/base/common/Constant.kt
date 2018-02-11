6467cf7ebc7189b9a7e998531183331bced81742
fjw <1281043316@qq.com>
2018-02-11 19:21:45 +0800

    RetrofitFactoryâ

---
+++ BaseLibrary/src/main/java/com/kotlin/base/common/Constant.kt
@@ -0,0 +1,11 @@
+package com.kotlin.base.common
+
+/**
+ * Created by Administrator on 2018/2/11/011.
+ */
+class Constant {
+
+    companion object {
+        val SERVER_ADDRESS = "http://120.79.59.193:8080/Kotlin_Server/"
+    }
+}
---
+++ BaseLibrary/src/main/java/com/kotlin/base/data/net/RetrofitFactory.kt
@@ -0,0 +1,74 @@
+package com.kotlin.base.data.net
+
+import com.kotlin.base.common.Constant
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by Administrator on 2018/2/11/011.
+ */
+class RetrofitFactory private constructor() {
+
+    /*
+       单例实现
+    */
+    companion object {
+        val instance: RetrofitFactory by lazy { RetrofitFactory() }
+    }
+
+    private val interceptor: Interceptor
+    private var retrofit: Retrofit
+
+
+    //初始化
+    init {
+
+        //通用拦截
+        interceptor = Interceptor {
+            chain -> val request = chain.request()
+                .newBuilder()
+                .addHeader("Content_Type","application/json")
+                .addHeader("charset","UTF-8")
+                .build()
+
+            chain.proceed(request)
+        }
+        retrofit = Retrofit.Builder()
+                .baseUrl(Constant.SERVER_ADDRESS)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .client(initClient())
+                .build()
+    }
+
+    private fun initClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+                .addInterceptor(initLogInterceptor())
+                .addInterceptor(interceptor)
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10,TimeUnit.SECONDS)
+                .build()
+    }
+
+
+    /*
+       日志拦截器
+    */
+    private fun initLogInterceptor(): HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return interceptor
+    }
+
+    /*
+        具体服务实例化
+     */
+    fun <T> create(service:Class<T>):T{
+        return retrofit.create(service)
+    }
+}
