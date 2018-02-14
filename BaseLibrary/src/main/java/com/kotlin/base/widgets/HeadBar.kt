921d38f13d44bdd250a6c91325014fd0a3257806
fjw <1281043316@qq.com>
2018-02-14 21:20:48 +0800

    HeadBar封装

---
+++ BaseLibrary/src/main/java/com/kotlin/base/widgets/HeadBar.kt
@@ -0,0 +1,44 @@
+package com.kotlin.base.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import com.kotlin.base.R
+import kotlinx.android.synthetic.main.layout_header_bar.view.*
+
+/**
+ * Created by Administrator on 2018/2/14/014.
+ */
+class HeadBar @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private var isShowBack = true
+    private var titleText: String? = null
+    private var rightText: String? = null
+
+    init {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.HeaderBar)
+        isShowBack = typedArray.getBoolean(R.styleable.HeaderBar_isShowBack, true)
+        titleText = typedArray.getString(R.styleable.HeaderBar_titleText)
+        rightText = typedArray.getString(R.styleable.HeaderBar_rightText)
+
+        initView()
+        //在 Android 自定义 View 的时候，需要使用 TypedArray 来获取 XML layout 中的属性值，使用完之后，需要调用 recyle() 方法将 TypedArray 回收。
+        typedArray.recycle()
+    }
+
+    private fun initView() {
+        View.inflate(context, R.layout.layout_header_bar, this)
+        //显示/隐藏
+        mLeftIv.visibility = if (isShowBack) View.VISIBLE else View.GONE
+        titleText?.let {
+            mTitleTv.text = it
+        }
+        rightText?.let {
+            mRightTv.text = it
+            mRightTv.visibility = View.VISIBLE
+        }
+
+    }
+}
