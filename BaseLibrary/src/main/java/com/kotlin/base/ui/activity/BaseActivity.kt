55620c35a98ef23286826065e1c23545cded98d6
fjw <1281043316@qq.com>
2018-02-14 16:56:35 +0800

    AppManager管理栈结构，退出功能

--- BaseLibrary/src/main/java/com/kotlin/base/ui/activity/BaseActivity.kt
+++ BaseLibrary/src/main/java/com/kotlin/base/ui/activity/BaseActivity.kt
@@ -1,10 +1,22 @@
 package com.kotlin.base.ui.activity
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.kotlin.base.common.AppManager
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity
 
 /**
  * Created by Administrator on 2018/2/11/011.
  */
 open class BaseActivity : RxAppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppManager.instance.addActivity(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        AppManager.instance.finishActivity(this)
+    }
 }
--- UserCenter/src/main/java/com/kotlin/user/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/kotlin/user/ui/activity/RegisterActivity.kt
@@ -1,6 +1,7 @@
 package com.kotlin.user.ui.activity
 
 import android.os.Bundle
+import com.kotlin.base.common.AppManager
 import com.kotlin.base.ext.onClick
 import com.kotlin.base.ui.activity.BaseMvpActivity
 import com.kotlin.user.R
@@ -13,6 +14,8 @@ import org.jetbrains.anko.toast
 
 
 class RegisterActivity : BaseMvpActivity<RegisterPresenter>(), RegisterView {
+
+    private var pressTime: Long = 0
 
     override fun injectionComponent() {
         DaggerUserComponent.builder().activityComponent(activityComponent).userModule(UserModule()).build().inject(this)
@@ -33,5 +36,14 @@ class RegisterActivity : BaseMvpActivity<RegisterPresenter>(), RegisterView {
         }
     }
 
+    override fun onBackPressed() {
+        val time = System.currentTimeMillis()
+        if (time - pressTime > 2000) {
+            toast("在按一次退出程序")
+            pressTime = time
+        } else {
+            AppManager.instance.exitApp(this)
+        }
+    }
 
 }
